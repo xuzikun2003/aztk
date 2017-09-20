b60d4966bedc63993c6f4a9b51f715c4755a7036
Pablo Selem <da_spaz@hotmail.com>
2017-09-19 23:21:34 -0700

    initial adls work

--- node_scripts/setup_wasb.sh
+++ node_scripts/setup_wasb.sh
@@ -4,8 +4,8 @@
 # Usage:
 # setup_wasb.sh [storage_account_name] [storage_account_key]
 
-storage_account_name=$1 
-storage_account_key=$2 
+storage_account_name=$1
+storage_account_key=$2
 storage_account_suffix=$3
 
 spark_home=/home/spark-current
@@ -14,7 +14,7 @@ cd $spark_home/conf
 cp spark-defaults.conf.template spark-defaults.conf
 
 cat >> spark-defaults.conf <<EOF
-spark.jars                 $spark_home/jars/azure-storage-2.0.0.jar,$spark_home/jars/hadoop-azure-2.7.3.jar
+spark.jars                 $spark_home/jars/azure-storage-2.0.0.jar,$spark_home/jars/hadoop-azure-2.7.3.jar,$spark_home/jars/azure-data-lake-store-sdk-2.0.11.jar,$spark_home/jars/hadoop-azure-datalake-3.0.0-alpha4.jar
 EOF
 
 cat >> core-site.xml <<EOF
@@ -22,13 +22,39 @@ cat >> core-site.xml <<EOF
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
 <property>
-<name>fs.AbstractFileSystem.wasb.Impl</name>
-<value>org.apache.hadoop.fs.azure.Wasb</value>
+  <name>fs.AbstractFileSystem.wasb.Impl</name>
+  <value>org.apache.hadoop.fs.azure.Wasb</value>
 </property>
 <property>
-<name>fs.azure.account.key.$storage_account_name.blob.$storage_account_suffix</name>
-<value>$storage_account_key</value>
+  <name>fs.azure.account.key.$storage_account_name.blob.$storage_account_suffix</name>
+  <value>$storage_account_key</value>
 </property>
+
+
+<property>
+  <name>fs.adl.impl</name>
+  <value>org.apache.hadoop.fs.adl.AdlFileSystem</value>
+</property>
+<property>
+  <name>fs.AbstractFileSystem.adl.impl</name>
+  <value>org.apache.hadoop.fs.adl.Adl</value>
+</property>
+
+<property>
+  <name>dfs.adls.oauth2.refresh.url</name>
+  <value>URL</value>
+</property>
+
+<property>
+  <name>dfs.adls.oauth2.client.id</name>
+  <value>ID</value>
+</property>
+
+<property>
+  <name>dfs.adls.oauth2.credential</name>
+  <value>KEY</value>
+</property>
+
 </configuration>
 EOF
 
@@ -38,3 +64,5 @@ cd $spark_home/jars
 apt install wget
 wget http://repo1.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar
 wget http://repo1.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.3/hadoop-azure-2.7.3.jar
+wget http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure-datalake/3.0.0-alpha4/hadoop-azure-datalake-3.0.0-alpha4.jar
+wget http://central.maven.org/maven2/com/microsoft/azure/azure-data-lake-store-sdk/2.0.11/azure-data-lake-store-sdk-2.0.11.jar
